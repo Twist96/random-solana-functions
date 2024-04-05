012f68c9120413122e56a3effc782714d6ac3633
Matthew Chukwuemeka <martins.twist@gmail.com>
2024-04-05 22:12:16 +0100

    add solana program

--- downtown_program/main.ts
+++ downtown_program/main.ts
@@ -1,10 +1,10 @@
 import idl from "./downtown_program.json"
+import { DowntownProgram } from "./downtown_program"
 import { Connection, Keypair, PublicKey, sendAndConfirmTransaction } from "@solana/web3.js"
 import { Program, Idl, AnchorProvider, setProvider, Wallet } from "@coral-xyz/anchor"
 import { loadKeypair } from "../MetaplexLogic"
 import { umi } from "../MetaplexLogic"
-
-console.log({idl})
+import { base58 } from "@metaplex-foundation/umi/serializers"
 
 async function main() {
     const _keypair = loadKeypair()
@@ -15,10 +15,24 @@ async function main() {
     setProvider(anchorProvider)
 
     const programId = new PublicKey(idl.metadata.address)
-    const program = new Program(idl as Idl, programId)
+    const program = ((new Program(idl as Idl, programId)) as unknown) as Program<DowntownProgram>
 
     //init contract
-    const tx = await program.methods.initialize().accounts({}).transaction()
-    const txSig = await sendAndConfirmTransaction(connection, tx, [keypair])
-    console.log({txSig})
+    const [town] = PublicKey.findProgramAddressSync([Buffer.from("town")], programId);
+    const tx = await program.methods.createTown("downtown").accounts({signer: keypair.publicKey, town}).transaction()
+    // const txSig = await sendAndConfirmTransaction(connection, tx, [keypair])
+    // console.log({txSig})
+
+    const secret = base58.deserialize(_keypair.secretKey)
+    console.log({secret})
+    console.log({pubkey: _keypair.publicKey})
 }
+
+main()
+.then(_ => {
+    console.log("DONE")
+})
+.catch(err => {
+    console.log({err})
+    console.log("FAILURE")
+})
